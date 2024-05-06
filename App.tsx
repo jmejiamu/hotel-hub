f0ff983cbc57d209ad5261eac845d93807451e57
jmejiamu <jmejiamu@gmail.com>
2024-05-06 15:53:51 -0700

    feat/add-storybook

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+// const apiUrl = Constants.expoConfig.extra.storybookEnabled;
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -10,11 +12,18 @@ export default function App() {
   );
 }
 
+let AppEntryPoint = App;
+
+if (Constants.expoConfig.extra.storybookEnabled === "true") {
+  AppEntryPoint = require("./.storybook").default;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+export default AppEntryPoint;
