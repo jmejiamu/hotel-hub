06cf90f1247a8dcc5e8633ccb141b6960a640672
jmejiamu <jmejiamu@gmail.com>
2024-05-11 12:37:17 -0700

    feat/code-screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,2 @@
 export * from "./EmployeeCustomerScreen/index";
+export * from "./CompanyCodeScreen/index";
---
+++ src/theme/fontSize.ts
@@ -0,0 +1,6 @@
+export const fontSize = {
+  size_small: 15,
+  size_medium: 20,
+  size_large: 30,
+  size_xt_large: 60,
+};
