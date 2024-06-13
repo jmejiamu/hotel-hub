4ae62d78eb4d451ff7dcadcf1e8a7569fc80066c
jmejiamu <jmejiamu@gmail.com>
2024-06-13 16:14:33 -0700

    feat/anable-front-end-destk to edit

--- src/screens/FrontEndCalendar/FrontendCalendar.tsx
+++ src/screens/FrontEndCalendar/FrontendCalendar.tsx
@@ -1,22 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { frontendCalendar } from "../../redux/frontEndDesk/frontEndDeskSlice";
+import { healerCalendar, healersSchedule, logoutUser } from "../../redux";
+import { EventItem, PackedEvent, RangeTime } from "@howljs/calendar-kit";
 import { AppDispatch, RootState } from "../../redux/ReduxStore/store";
+import { CalendarFooter, CustomCalendar } from "../../component";
 import { eventConfigs } from "../PredefineCalendar/eventConfig";
-import { TimelineCalendar } from "@howljs/calendar-kit";
+import { CalendarModal } from "../../component/CalendarModal";
+import { useNavigation } from "@react-navigation/native";
 import { generateYearlyEvents } from "../../utils";
+import { RootNavigationNames } from "../../types";
+import { useFadeAnimation } from "../../hooks";
 import { View, Image } from "react-native";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import uuid from "react-native-uuid";
 import { styles } from "./styles";
 
-export const FrontendCalendar = () => {
+export const FrontendCalendar = (props) => {
   const predefinedEvents = generateYearlyEvents(eventConfigs);
   const dispatch = useDispatch<AppDispatch>();
-  const { response } = useSelector(
+  const [selectedEvent, setSelectedEvent] = useState<PackedEvent>();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [events, setEvents] = useState<EventItem[]>([]);
+  const navigate = useNavigation<RootNavigationNames>();
+  const { fadeAnim } = useFadeAnimation(1000);
+  const [eventId, setEventId] = useState("");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const { setLogged } = props;
+  const [items, setItems] = useState([
+    { label: "none", value: "none" },
+    { label: "Room1", value: "room1" },
+    { label: "Room2", value: "room2" },
+    { label: "Room3", value: "room3" },
+  ]);
+  const { response: res } = useSelector(
     (state: RootState) => state.frontEndDeskCalendar
   );
 
-  const mapEvent = response.map((event) => {
+  const { response, error, loading } = useSelector(
+    (state: RootState) => state.userAuth
+  );
+
+  const mapEvent = res.map((event) => {
     return {
       id: event.event_id,
       title: event.event_title,
@@ -29,16 +55,137 @@ export const FrontendCalendar = () => {
   });
   const combinedEvents = [...predefinedEvents, ...mapEvent];
 
+  const _onDragCreateEnd = (event: RangeTime) => {
+    const event_id = uuid.v1();
+
+    const newEvent = {
+      id: String(event_id),
+      title: response.username,
+      start: event.start,
+      end: event.end,
+      color: "#B1AFFF",
+      description: "Hello world",
+      user_id: response.user_id,
+      userType: response.userType,
+    };
+    setEvents((prev) => [...prev, newEvent]);
+  };
+
+  const _onLongPressEvent = (event: PackedEvent) => {
+    setSelectedEvent(event);
+  };
+
+  const _onPressCancel = () => {
+    setSelectedEvent(undefined);
+  };
+
+  const _onPressSubmit = () => {
+    setEvents((prevEvents) =>
+      prevEvents.map((ev) => {
+        if (ev.id === selectedEvent?.id) {
+          //FIX: this has to change to specific user
+          dispatch(
+            //TODO: change this
+            healerCalendar({
+              event_id: selectedEvent.id,
+              user_id: response?.user_id,
+              userType: response?.userType,
+              eventTitle: selectedEvent?.title || "",
+              eventDescription: selectedEvent.description,
+              eventStartDate: selectedEvent.start,
+              eventEndDate: selectedEvent.end,
+              path: "healer-calendar",
+            })
+          );
+          return { ...ev, ...selectedEvent };
+        }
+        return ev;
+      })
+    );
+
+    setSelectedEvent(undefined);
+  };
+
+  const onHandleLogout = async () => {
+    try {
+      dispatch(logoutUser()).unwrap();
+      setLogged(false);
+      navigate.navigate("CustomerEmployeeScreen");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (response.user_id) {
+      //TODO: Double check this to we can see if we need to change
+      dispatch(
+        healersSchedule({
+          user_id: response.user_id,
+          userType: response.userType,
+        })
+      );
+    }
+  }, [response.user_id]);
+
+  useEffect(() => {
+    if (res.length > 0) {
+      //TODO: check this too
+      setEvents(
+        res.map((item) => ({
+          id: item.event_id,
+          title: item.event_title,
+          start: item.event_start,
+          end: item.event_end,
+          color: "#B1AFFF",
+          description: item.event_description,
+          user_id: item.user_id,
+          userType: item.userType,
+        }))
+      );
+    }
+  }, []);
   useEffect(() => {
     dispatch(frontendCalendar());
   }, []);
+
   return (
     <View style={styles.mainContainer}>
       <Image
         source={require("../../../assets/def-cal.jpg")}
         style={styles.headerImage}
       />
-      <TimelineCalendar viewMode="week" events={combinedEvents} />
+
+      <CustomCalendar
+        canlendarView="week"
+        events={combinedEvents}
+        onDragCreateEnd={_onDragCreateEnd}
+        onLongPressEvent={_onLongPressEvent}
+        selectedEvent={selectedEvent}
+        setSelectedEvent={setSelectedEvent}
+        setEventId={setEventId}
+        setModalVisible={setModalVisible}
+      />
+
+      {!!selectedEvent && (
+        <CalendarFooter
+          onPressCancel={_onPressCancel}
+          onPressSubmit={_onPressSubmit}
+        />
+      )}
+      <CalendarModal
+        animationType="fade"
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        events={events}
+        eventId={eventId}
+        setOpen={setOpen}
+        open={open}
+        value={value}
+        setValue={setValue}
+        items={items}
+        setItems={setItems}
+      />
     </View>
   );
 };
